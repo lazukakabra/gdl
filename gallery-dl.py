7221622332b8fa54a1db374822da835d934e654a
lazukakabra <n1h1lum29@gmail.com>
2026-08-30 13:57:01 +0200

    first commit

---
+++ gallery-dl.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+'''
+requirements and made with versions:
+[exe] python 3.14.6
+[exe] gallery-dl-1.32.9, installed with pip
+'''
+import os
+import subprocess
+from shutil import move
+import tempfile
+import gdl_req_check as grc
+
+# foreground color codes
+os.system('color') # needed for ANSI color codes to work (win10)
+GREEN, YELLOW, DARK_CYAN, RED_BG = '\033[92m', '\033[93m', '\033[36m', '\033[41m'
+RESET = '\033[0m'  # This resets the color back to default
+
+# color wrapping
+def color(color, arg:str)->str:
+	return color+arg+RESET
+
+# \t, indented print
+def print2(*arg):
+	for element in arg:
+		print(f'\t{element}')
+
+# if directory doesnt exist, create directory and print location
+def create_dir(path:str):
+	if not os.path.exists(path):
+		os.mkdir(path)
+		print2(color(DARK_CYAN, 'folder created at: ')+f'{path}')
+
+# finds folder containing files, can differ depending on
+# download settings set for different sites in config file
+def tree(path:str, folder='')->str:
+	if not os.path.isfile(path):
+		folder = tree(os.path.join(path, os.listdir(path)[0]), path)
+	return folder
+
+# check if filename exists in destination directory, assumes 3 letter extension file
+def check_duplicate(name:str, og_name:str, dest_dir:str, dl_dir_tree:str, n=0)->(str, int):
+	fname, fext = os.path.splitext(os.path.join(dl_dir_tree, og_name))
+	lext = len(fext)+1 
+	path_duplicate = os.path.join(dest_dir, dl_dir_tree, name)
+	if os.path.exists(path_duplicate):
+		name = fname + f' ({n+1})' + fext
+		name, n = check_duplicate(name, og_name, dest_dir, dl_dir_tree, n+1)
+	return name, n
+
+# download func, needs gallery-dl options and url,
+def download(options:dict, url:str):
+	flags = ''
+	for option in options:
+		flags += ' '+option
+	subprocess.check_output(f'gallery-dl{flags} {url}')
+
+# get size and return with proper unit
+def fsize(fname:str, fopath:str)->str:
+	size = os.path.getsize(os.path.join(fopath, fname))
+	units = ['B', 'kB', 'MB', 'GB']
+	unit_index = 0
+	for _ in units[-1]:
+		if size > 1024:
+			unit_index += 1
+			size = size / 1024.0
+	size = str(size)[:5] # return 3 decimals max, ignore rounding
+	return f'{size} {units[unit_index]}'
+
+# needs downloaded folder path
+def rename_and_move(dir_path:str, download_dir:str):
+	# change folders to tmp_dir
+	prev_dir = os.getcwd()
+	os.chdir(dir_path)
+
+	# find folder structure
+	folderpath = tree(os.listdir()[0])
+	filenames = os.listdir(folderpath)
+	old_filenames = filenames
+
+	print2(color(GREEN, 'downloaded:'), '    folder: '+folderpath)
+	for filename in filenames:
+		print2(
+			'      file: '+filename,
+			'      size: '+fsize(filename, folderpath))
+
+	# checking for duplicates in dl_dir and adjusting filenames if found
+	duplicates = 0
+	fname_duplicates = []
+	for i, fn in enumerate(filenames):
+		filenames[i], n_duplicate = check_duplicate(fn, fn, download_dir, folderpath)
+		if n_duplicate:
+			fname_duplicates.append((fn, n_duplicate))
+			duplicates += 1
+
+	# if duplicate found, ask to continue, if yes then >>
+	# rename and print msg with new name
+	if duplicates:
+		print2('found '+color(YELLOW, f'{duplicates}')+
+			f' out of '+color(DARK_CYAN, f'{len(filenames)}')+' duplicates')
+		for fn in fname_duplicates:
+			print2(f'found '+color(YELLOW, f'{fn[1]}')+
+				' duplicates in destination folder for: ', '  '+fn[0])
+		cont = input(color(YELLOW, '  duplicate found, continue? [Y/n] '))
+		if cont and cont.lower() not in['y', 'ye', 'yes']:
+			print2(color(YELLOW, 'process terminated >> deleting downloaded files...'))
+			os.chdir(prev_dir)
+			return
+		for ofn, fn in zip(old_filenames, filenames):
+			os.rename(os.path.join(folderpath, ofn), os.path.join(folderpath, fn))
+			print2(color(GREEN,'renamed >> ')+fn)
+	else:
+		print2('found '+color(YELLOW, '0')+' duplicates in destination folder')
+
+	# ensuring directory exists in destination folder
+	create_dir(os.path.join(download_dir, folderpath))
+
+	# moving file(s)
+	old_folder_path = os.path.join(dir_path, folderpath)
+	new_folder_path = os.path.join(download_dir, folderpath)
+	for ofn, fn in zip(old_filenames, filenames):
+		ofp = os.path.join(old_folder_path, ofn)
+		nfp = os.path.join(new_folder_path, fn)
+		move(ofp, nfp)
+		print2('', color(GREEN, f'moved >> {fn}'),
+		'  from: '+old_folder_path,
+		'    to: '+new_folder_path)
+
+	# if log not empty, move from download_dir\logs\ to
+	# new_folder_path\logs\filename.log, if multiple files downloaded
+	# simply use the 0th index from filenames to name the log
+	if os.path.getsize(LOG_FILE_PATH):
+		new_log_folder = os.path.join(new_folder_path, LOG_FOLDER)
+		create_dir(new_log_folder)
+		new_log_path = os.path.join(new_log_path, filenames[0]+'.log')
+		move(LOG_FILE_PATH, new_log_path)
+		print2(color(RED_BG, 'non-empty log file, saved:'), new_log_path,'')
+	else:
+		print2(color(GREEN, 'empty log file, deleting...'))
+		os.remove(LOG_FILE_PATH)
+	os.rmdir(LOG_FOLDER_PATH)
+
+	# move back to prev dir to safely delete temporary directory
+	os.chdir(prev_dir)
+
+# gets download folder paths from file if it exists, creates file if it does not
+def check_file_for_paths(filename:str)->dict:
+	# if file doesnt exist, create it and add file save locations
+	script_dir = os.path.dirname(os.path.realpath(__file__))
+	prv_dir = os.getcwd()
+	os.chdir(script_dir)
+	if not os.path.exists(filename):
+		print2('[ATTN] >> file containing save locations '+color(RED_BG, 'not')+' found, creating...')
+		locations = {}
+		while True:
+			locations['download'] = input('\t[INPUT] >> enter location to save videos, full path: ')
+			for k,v in locations.items():
+				print2('\t  '+k+': '+v)
+			q = input('\t[INPUT] >> is the above folder correct? [Y/n] ')
+			if q and str(q).lower() not in ['y', 'ye', 'yes']:
+				continue
+			break
+
+		# write to file the entered locations
+		with open(filename, 'x') as f:
+			for k, v in locations.items():
+				f.write(k+' = '+v + '\n')
+		print2(color(GREEN, 'created file >> ')+filename)
+
+	# get file paths from filename
+	locs = {}
+	with open(filename) as f:
+		contents = f.read()
+	for line in contents.split('\n'):
+		if not line: continue
+		k, v = line.split(' = ')
+		locs[k] = v
+
+	# print locations and return dict
+	print2(color(GREEN, 'found ')+f'{filename}, '
+		+color(DARK_CYAN, 'download folder set to:'),
+		color(DARK_CYAN, '  >> ')+locs['download'],
+		f'  to change, edit {filename} or delete it and rerun script.')
+	os.chdir(prv_dir)
+	return locs
+
+''' FLAGS for gallery-dl, explanations
+-d <path>:	exact location for file downloads, leave alone below if its not needed to change
+			files will be downloaded to temporary directory first. this is to primarily 
+			avoid overwrites and to facilitate easier renaming.
+--windows-filenames: force filenames to be Windows-compatible,
+					 comment out only if not on windows.
+-c <path>: exact location of config file.
+-w:	print only warnings and errors.
+-q:	activate quiet mode.
+'''
+
+## static filenames and paths
+# set filename containing save-folder paths
+paths_file = 'gdl_paths.txt'
+
+# download
+locations = check_file_for_paths(paths_file)
+DOWNLOAD_DIR = Rf'{locations['download']}'
+
+# config
+CONFIG_NAME = 'gallery-dl.conf'
+LOC_THIS_SCRIPT = os.path.dirname(__file__)
+CONFIG_PATH = os.path.join(LOC_THIS_SCRIPT, CONFIG_NAME)
+
+# name of log set in config file, set to save at: cwd/logs/log.log
+LOG_FILE = R'log.log'
+LOG_FOLDER = 'logs'
+LOG_FOLDER_PATH = os.path.join(DOWNLOAD_DIR, LOG_FOLDER)
+LOG_FILE_PATH = os.path.join(LOG_FOLDER_PATH, LOG_FILE)
+
+# if -d and/or -c is used below, then remove from main(),
+# -d currently set to use temporary directory to avoid
+# overwriting potential duplicates
+gdl_opts = [
+			#f'-d {DL_DIR}',
+			f'-c {CONFIG_PATH}',
+			'--windows-filenames',
+			'-w',
+			#'-q ',
+			]
+
+def main():
+	# checking for requirements with helper script ytdlp_req_check.py
+	if not grc.report():
+		print2(color(GREEN, 'found')+' all dependencies')
+	else:
+		grc.main()
+		input('press enter to quit')
+		quit()
+
+	# to make logs spawn at expected location
+	# regardless of where you execute script from
+	prv_dir = os.getcwd()
+	os.chdir(DOWNLOAD_DIR)
+
+	while True:
+		get_url = input(color(DARK_CYAN, '  [gdl] >> enter url or leave empty to exit. '))
+		if not get_url:
+			os.chdir(prv_dir)
+			quit()
+		with tempfile.TemporaryDirectory() as tmp_dir:
+			gdl_opts.append(f'-d {tmp_dir}')
+			download(gdl_opts, get_url)
+			rename_and_move(tmp_dir, DOWNLOAD_DIR)
+			
+if __name__ == '__main__':
+	main()
---
+++ gdl_req_check.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+''' use the script to check if dependencies for gallery-dl.py
+exists on system'''
+
+import os
+from importlib.util import find_spec
+from importlib.metadata import version
+from shutil import which
+from subprocess import run
+from platform import uname
+
+# set requirements to check here
+libs = ['requests', 'gallery_dl']
+
+# optional dependencies. needed if gallery-dl is requested to download video
+# which relies on yt-dlp which in turn needs the rest for some, not all, formats
+# there are more optional dependencies, add them if needed, see:
+# https://github.com/mikf/gallery-dl#dependencies
+opt_libs = ['yt_dlp', 'yt_dlp_ejs', 'mutagen']	# python libs
+opt_exe_fftools = ['ffmpeg', 'ffprobe']	# fftools gives alot of version info, separated
+opt_exe = ['deno']	# installed on system
+
+# status dict will be key=name and value=[requirement yes or no, lib or exe, version]
+# if no version, eg. "name" wasnt found. then save '' (empty str)
+status = {}
+
+# used as true/false with report() when called by other scripts.
+# also used as giving a total in table print
+not_found_req = 0
+not_found_opt = 0
+
+# prefix for printing, added to status dict when making entry to dict
+l, e, r, o = '[LIB]', '[EXE]', '[REQUIRED]', '[OPTIONAL]'
+
+# checks for python libs
+def check_lib(status_dict, list_of_libs, dependency, typ, missing=0):
+	for lib in list_of_libs:
+		status_dict[lib] = [dependency, typ, '']
+		if find_spec(lib) is None:
+			missing += 1
+		else:
+			status_dict[lib][-1] = version(lib)
+	return status_dict, missing
+
+# for fftools, no direct way to get only version
+# so it has to be dug out of a small wall of text
+def check_fftools(status_dict, list_of_exes, dependency, typ, missing=0):
+	for exe in list_of_exes:
+		status_dict[exe] = [dependency, typ, '']
+		if bool(which(exe)):
+			exe_version = run([exe, '-version'], capture_output=True, text=True).stdout
+			vers = ''.join(char for char in exe_version)
+			vers = vers.split(' Copyright')[0].split('version ')[1]
+			status_dict[exe][-1] = vers
+		else:
+			missing += 1
+	return status_dict, missing
+
+# checks for executeables which prints with "name -version" as "<name> <version>"
+def check_exe(status_dict, list_of_exes, dependency, typ, missing=0):
+	for exe in list_of_exes:
+		status_dict[exe] = [dependency, typ, '']
+		if bool(which(exe)):
+			exe_version = run([exe, '-version'], capture_output=True, text=True).stdout
+			vers = ''.join(char for char in exe_version)
+			vers = vers.strip('\n').split('deno ')[1]
+			status[exe][-1] = vers
+		else:
+			missing += 1
+	return status_dict, missing
+
+status, n1 = check_lib(status, libs, r, l)
+status, n2 = check_lib(status, opt_libs, o, l)
+status, n3 = check_fftools(status, opt_exe_fftools, o, e)
+status, n4 = check_exe(status, opt_exe, o, e)
+
+not_found_req += n1
+not_found_opt += n2+n3+n4
+
+# report func to be called from ytdlp.py, will not work if called from py console
+# returns true if any requirements are not found
+def report():
+	return bool(not_found_req)
+
+def main():
+	# foreground color codes
+	os.system('color') # needed for ANSI color codes to work (win10)
+	GREEN, YELLOW, DARK_CYAN, RED_BG = '\033[92m', '\033[93m', '\033[36m', '\033[41m'
+	RESET = '\033[0m'  # This resets the color back to default
+
+	# color input and reset cmd style to default
+	def color(color:str, arg:str) -> str:
+		return color+arg+RESET
+
+	# \t, indented print
+	def print2(*arg):
+		for element in arg:
+			print(f'\t{element}')
+
+	# printing status msgs
+	print2('', color(DARK_CYAN, 'requirements table for gallery-dl.py'))
+
+	header = color(YELLOW, f'{"[status]":<11}{"[dependency]":<14}{"[type]":<7}{"name":14}version')
+	separater_row = '-'*len(header)
+	print2(separater_row, header, separater_row)
+
+	for pkg, values in status.items():
+		dep, typ, ver = values
+		if not ver:
+			print2(color(RED_BG, f'{"[- FOUND]":<11}{dep:<14}{typ:<7}{pkg:14}{ver}'))
+
+		else:
+			print2(color(GREEN, f'{"[+ FOUND]":<11}{dep:<14}{typ:<7}{pkg:14}{ver}'))
+
+	print2(separater_row)
+	print2(color(YELLOW, f'missing {not_found_req} required dependencies'),
+		color(YELLOW, f'missing {not_found_opt} required dependencies'))
+	print2(color(DARK_CYAN, f'your system >> ')+uname()[0])
+
+if __name__ == '__main__':
+	main()
